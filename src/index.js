d008679f92413b43376f83a05664df5e5ff38525
mshankar0101 <madhushankar0101@gmail.com>
2024-03-06 01:27:38 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 const app = require('./app');
 const dotenv = require('dotenv');
-
-
 dotenv.config();
-
-
-app.listen(3000, () => console.log('Server running......'));
+app.listen(3011, () => console.log('Server running......'));
 
